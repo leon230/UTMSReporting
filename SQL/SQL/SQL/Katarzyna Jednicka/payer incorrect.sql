d93708a98c3351a4ea91dd8cfd867b43819178c5
lukasz.homik <homik.lukasz@gmail.com>
2016-08-05 09:13:09 +0200

    -500 corection

--- SQL/SQL/SQL/Katarzyna Jednicka/payer incorrect.sql
+++ SQL/SQL/SQL/Katarzyna Jednicka/payer incorrect.sql
@@ -7,6 +7,11 @@ where loc.location_gid = sh.servprov_gid
 )													tsp_name
 ,sh.TOTAL_ACTUAL_COST								shipment_cost
 ,OM.ORDER_RELEASE_GID
+,(select SS.status_value_gid from
+shipment_status ss
+					where ss.shipment_gid = sh.shipment_gid
+
+                    and ss.status_type_gid = 'ULE/PR.SHIPMENT_COST')
 
 
 
@@ -31,7 +36,9 @@ AND EXISTS
 		where OM1.order_release_gid = ORLS_REF.order_release_gid
 		and OM1.SHIPMENT_GID = SH.SHIPMENT_GID
 		and orls_ref.ORDER_RELEASE_REFNUM_QUAL_GID  IN ('ULE.ULE_LEG1_PAYER','ULE.ULE_LEG2_PAYER')
-		
+
 		)
+
+
 		-- AND SH.SHIPMENT_GID = 'ULE/PR.101806963'
 AND TO_CHAR(SH.INSERT_DATE,'YYYY') = '2016'
